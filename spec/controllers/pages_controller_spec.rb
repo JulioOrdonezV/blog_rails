5544e4520688dc6864835fc4929da4eb052fb47c
Julio Ordonez Viales <julio7k@gmail.com>
2015-08-11 15:19:32 -0600

    Add pages controller and user model test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  describe "GET #index" do
+    it "retuns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  before(:each) do
+    @attr = { provider: 'facebook', uid: '1234567', name: 'John Doe' }
+  end
+
+  it "should create a new user given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(name: ''))
+    expect(no_name_user).to be_invalid
+  end
+
+  it "should have a provider" do
+    no_provider_user = User.new(@attr.merge(provider: ''))
+    expect(no_provider_user).to be_invalid
+  end
+
+  it "should have an uid" do
+    no_uid_user = User.new(@attr.merge(uid: ''))
+    expect(no_uid_user).to be_invalid
+  end
 end
