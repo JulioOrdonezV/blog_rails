b995155dff5c0e36c3060bd8e7b3608446445334
Julio Ordonez Viales <julio7k@gmail.com>
2015-10-05 11:42:54 -0600

    Handle errors with flash messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(comment_params)
     @comment.user_id = current_user.id
-    @comment.save!
+    flash[:error] = @comment.errors if @comment.errors
+    @comment.save
     redirect_to article_path(@article)
   end
 
@@ -15,7 +16,9 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
+
   private
+
     def comment_params
       params.require(:comment).permit(:body)
     end
